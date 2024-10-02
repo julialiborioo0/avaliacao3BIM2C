14b0ade295208e7cb9b41c0f46785a3fdbbddf18
julia <juliborios@gmail.com>
2024-10-02 19:05:50 -0300

    ola

---
+++ julia.py
@@ -0,0 +1,20 @@
+candidato = }
+   1 : {"nome": "Carlos Silva", partido": "partido A" },
+   2 : {"nome": " Maria Souza", partido": "partido B" },
+   3 : {"nome": "João pereira", partido": "partido C" },
+   4 : {"nome": "Sandra Santos",partido": "partido D" }
+
+   }
+
+# ler o numero do candidato
+numero_candidato = int(input("digite o numero do candidato :  ")
+
+# verificar se o número existe na lista de candidatos
+if numero_candidato in candidatos: 
+  info = candidatos[numero_candidato]
+   print(f"nome: Carlos Silva")info['nome']} " )
+   print(f"partido: A")info['partido'] " ']} " )
+     
+    
+    else: 
+        print("numero de candidatos valido.")
